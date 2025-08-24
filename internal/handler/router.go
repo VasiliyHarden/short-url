667cf36992aa0bd564bbf5b27048700b27c23cae
VasiliyHarden <55534134+VasiliyHarden@users.noreply.github.com>
2025-08-24 23:10:44 +0400

    Инкремент 6: логирование сведений о запросах и ответах (#6)

--- internal/handler/router.go
+++ internal/handler/router.go
@@ -1,13 +1,16 @@
 package handler
 
 import (
+	"github.com/VasiliyHarden/short-url/internal/handler/middleware"
 	"github.com/VasiliyHarden/short-url/internal/service/shortener"
 	"github.com/go-chi/chi/v5"
+	"go.uber.org/zap"
 	"net/http"
 )
 
-func NewRouter(sh *shortener.Service) http.Handler {
+func NewRouter(sh *shortener.Service, logger *zap.Logger) http.Handler {
 	r := chi.NewRouter()
+	r.Use(middleware.Logging(logger))
 	r.Get("/{code}", Resolve(sh))
 	r.Post("/", Shorten(sh))
 
